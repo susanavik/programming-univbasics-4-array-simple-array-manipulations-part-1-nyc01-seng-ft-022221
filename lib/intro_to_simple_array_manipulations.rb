a6f82d35a08fb3204ce063a8431bc8c1e5cf2456
Susana vik <susana.m.vik@gmail.com>
2021-02-04 18:58:52 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,29 @@
+def using_push(array, string)
+  array = ["this", "is", "a"]
+  array.push("string")
+end
+
+def using_unshift(array, string)
+    array = ["this", "is", "a"]
+    array.unshift("string")
+end
+
+def using_pop(array, string)
+  array = ["this", "is", "a"]
+  a = array.pop
+end
+
+def pop_with_args(array, string)
+  array = ["this", "is", "a"]
+  array.pop[2]
+end
+
+def using_shift(array, string)
+  array = ["this", "is", "a"]
+  this = array.shift
+end
+
+def shift_with_args(array, string)
+  array = ["this", "is", "a"]
+  array.shift[2]
+end
